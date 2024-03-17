7250131b70726f1eb63b3acd5ecc2496fd7e1c73
ccolorcat <ch.xx@outlook.com>
2024-03-17 12:46:26 +0800

    build: mod publish script

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,3 +6,8 @@ repositories {
     gradlePluginPortal()
     google()
 }
+
+dependencies {
+    implementation("com.android.tools.build:gradle:8.2.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+}
