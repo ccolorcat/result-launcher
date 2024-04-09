e62f86d9c5f2bd8e220bb262eb7c6ff4c2239d6e
ccolorcat <ch.xx@outlook.com>
2024-04-10 00:31:28 +0800

    build: downgrade gradle

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -8,22 +8,24 @@ import org.gradle.api.JavaVersion
 object BuildConfig {
     const val groupId = "cc.colorcat.runtime"
 
-    const val compileSdk = 34
+    const val compileSdk = 33
     const val minSdk = 23
     const val targetSdk = compileSdk
 
-    const val versionCode = 7
-    const val versionName = "2.1.0"
+    const val versionCode = 10
+    const val versionName = "2.1.3"
 
-    val javaVersion = JavaVersion.VERSION_17
-    const val jvmTarget = "17"
+    val javaVersion = JavaVersion.VERSION_1_8
+    const val jvmTarget = "1.8"
 }
 
 object Libs {
     object Remote {
-        const val coreKtx = "androidx.core:core-ktx:1.12.0"
-        const val appcompat = "androidx.appcompat:appcompat:1.6.1"
-        const val material = "com.google.android.material:material:1.11.0"
+        const val coreKtx = "androidx.core:core-ktx:1.10.1"
+        const val appcompat = "androidx.appcompat:appcompat:1.3.1"
+        const val material = "com.google.android.material:material:1.4.0"
+        const val activityKtx = "androidx.activity:activity-ktx:1.7.2"
+        const val fragmentKtx = "androidx.fragment:fragment-ktx:1.5.5"
         const val kotlinxCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3"
         const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.6.2"
     }
